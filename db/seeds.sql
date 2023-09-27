80d4e4ba152b7a8aa902d50b1aed9c6e7f14152c
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-09-27 11:15:24 -0500

    fixed add department and add employee functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO department (id, name)
-VALUES (1, "Sales"),
-(2, "Engineering"),
-(3, "Finance"),
-(4, "Legal");
+INSERT INTO department  (name)
+VALUES ("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
 
 
 INSERT INTO role (id, title, salary, department_id)
--- index.js
+++ index.js
@@ -2,18 +2,14 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const util = require("util");
 
-
-const db = mysql.createConnection(
-  {
-    host: "127.0.0.1",
-    user: "root",
-    password: "roo1234",
-    database: "employees_db",
-  }
-);
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "roo1234",
+  database: "employees_db",
+});
 
 const query = util.promisify(db.query).bind(db);
-
 
 const initialQuestion = [
   {
@@ -28,7 +24,7 @@ const initialQuestion = [
       "Add Role",
       "Add Employee",
       // "Update Employee",
-      "Quit"
+      "Quit",
     ],
   },
 ];
@@ -54,91 +50,136 @@ const init = async () => {
     case "Add Employee":
       addEmployee();
       break;
+    case "Update Employee":
+      updateEmployee();
     case "Quit":
       quit();
       break;
     default:
-      console.log("This is broken.")
-      // process.exit();
+      console.log("This is broken.");
+    // process.exit();
   }
 };
 const viewDepartment = async () => {
-const result = await query("SELECT * FROM department")
+  const result = await query("SELECT * FROM department");
   console.table(result);
   init();
 };
 
 const viewRoles = async () => {
-  const roleResult = await query ("SELECT * FROM role")
+  const roleResult = await query("SELECT * FROM role");
   console.table(roleResult);
   init();
 };
 
-//SHOULD THIS HAVE MANAGERS NAMES INSTEAD OF THE MANAGER's EMPLOYEE ID 
+//SHOULD THIS HAVE MANAGERS NAMES INSTEAD OF THE MANAGER's EMPLOYEE ID
 const viewEmployees = async () => {
-  const employeeResult = await query ("SELECT * FROM employee")
+  const employeeResult = await query("SELECT * FROM employee");
   console.table(employeeResult);
   init();
 };
 
-//UPDATING AS A NUMBER RATHER THAN THE INPUTTED TEXT "0"
+//UPDATING AS A STRING OF "0" RATHER THAN THE INPUTTED TEXT it's fixed now don't worry :-)
+
 const addDepartment = async () => {
-  // const newDepartment = await query("SELECT name AS value FROM department")
   const addDepartmentQuestions = [
     {
       type: "input",
       message: "Type in the name of the new department.",
-      name: "name"
-    }
-  ]
-  const answer = await inquirer.prompt(addDepartmentQuestions);
-  await query("INSERT INTO department (name) VALUES (?)", answer);
+      name: "name",
+    },
+  ];
+  const { name } = await inquirer.prompt(addDepartmentQuestions);
+  await query("INSERT INTO department (name) VALUES(?)", [name]);
   viewDepartment();
-}
+};
 
 const addRole = async () => {
-const departments = await query("SELECT id AS value, name FROM department") 
-const addRoleQuestions = [
-  {
-    type: "input",
-    message:"Input the role name.",
-    name: "title"
-  },
-  {
-    type: "input",
-    message: "Input the salary amount.",
-    name: "salary"
-  },
-  {
-    type: "list",
-    message: "Select the department this role is within.",
-    name: "department_id",
-    choices: departments
-  }
-]
-const {title, salary, department_id} = await inquirer.prompt(addRoleQuestions);
-await query ("INSERT INTO role (title, salary, department_id) VALUES (?,?,?)", [title, parseInt(salary), department_id])
-viewRole();
+  const departments = await query("SELECT id AS value, name FROM department");
+  const addRoleQuestions = [
+    {
+      type: "input",
+      message: "Input the role name.",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Input the salary amount.",
+      name: "salary",
+    },
+    {
+      type: "list",
+      message: "Select the department this role is within.",
+      name: "department_id",
+      choices: departments,
+    },
+  ];
+  const { title, salary, department_id } = await inquirer.prompt(
+    addRoleQuestions
+  );
+  await query(
+    "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)",
+    [title, parseInt(salary), department_id]
+  );
+  viewRoles();
 };
 
 addEmployee = async () => {
-  const roles = await query("SELECT id AS value, title FROM role")
+  const roles = await query("SELECT title AS name, id AS value FROM role");
+  const managers = await query(
+    "SELECT CONCAT(first_name, last_name) as name, id AS value FROM employee WHERE manager_id IS null"
+  );
+  console.log(roles);
   const addEmployeeQuestions = [
     {
       type: "input",
       message: "Input the employee's first name.",
-      name: "first_name"
+      name: "first_name",
     },
     {
       type: "input",
-      message: "Input the employee's last name."
-    }
-  ]
-}
+      message: "Input the employee's last name.",
+      name: "last_name",
+    },
+    {
+      type: "list",
+      message: "Select the title this employee holds.",
+      name: "role_id",
+      choices: roles,
+    },
+    {
+      type: "list",
+      message: "Select this employee's manager.",
+      name: "manager_id",
+      choices: managers,
+    },
+  ];
+  const { first_name, last_name, role_id, manager_id } = await inquirer.prompt(
+    addEmployeeQuestions
+  );
+  await query(
+    "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?)",
+    [first_name, last_name, role_id, manager_id]
+  );
+  viewEmployees();
+};
+
+updateEmployee = async () => {
+  const employeeUpdateOptions = await query(
+    "SELECT CONCAT(first_name, last_name) as name, id AS value, FROM employee"
+  );
+  const updateEmployeeQuestions = [
+    {
+      type: "list",
+      message: "Select the employee you would like to work with.",
+      name: "",
+    },
+  ];
+};
 
 const quit = () => {
-console.log("Goodbye.")
-  process.exit() 
-}
+  console.log("Goodbye.");
+  process.exit();
+};
 
 init();
